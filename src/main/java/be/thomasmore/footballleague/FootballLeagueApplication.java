8518ef41d225cebc4357d001178ed42436d28942
salihtzc <salihtzc.1997@gmail.com>
2023-03-01 18:03:12 +0100

    first commit

---
+++ src/main/java/be/thomasmore/footballleague/FootballLeagueApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.footballleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballLeagueApplication.class, args);
+    }
+
+}
