5970e3497e961040f478d95593b5e966b7240ff7
salihtzc <salihtzc.1997@gmail.com>
2023-03-09 13:38:48 +0100

    data added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO "PUBLIC"."TEAM" VALUES
+    (1, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/10.png', 'Manchester City', 85),
+    (2, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/9.png', 'Liverpool', 84),
+    (3, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/5.png', 'Chelsea', 83),
+    (4, 'https://assets.stickpng.com/images/580b57fcd9996e24bc43c4ee.png', 'Spurs', 82),
+    (5, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/11.png', 'Manchester Utd', 82),
+    (6, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/1.png', 'Arsenal', 81),
+    (7, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/95.png', 'Leicester City', 79),
+    (8, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/19.png', 'West Ham', 79),
+    (9, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/13.png', 'Newcastle Utd', 79),
+    (10, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/2.png', 'Aston Villa', 79),
+    (11, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/110.png', 'Wolves', 78),
+    (12, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/7.png', 'Everton', 77),
+    (13, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/14.png', 'Nottingham Forest', 77),
+    (14, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/1799.png', 'Crystal Palace', 76),
+    (15, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/8.png', 'Leeds United', 76),
+    (16, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/144.png', 'Fulham', 76),
+    (17, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/17.png', 'Southampton', 76),
+    (18, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/1808.png', 'Brighton', 76),
+    (19, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/1925.png', 'Brentford', 75),
+    (20, 'https://fifastatic.fifaindex.com/FIFA23/teams/dark/1943.png', 'AFC Bournemouth', 74);
+
+
